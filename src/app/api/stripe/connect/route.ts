886fcb2f714d788c72f581190398de4ca261233f
Ganesh Kumar <157221663+gan3xh@users.noreply.github.com>
2024-07-28 23:59:49 +0530

    configured stripe

--- src/app/api/stripe/connect/route.ts
+++ src/app/api/stripe/connect/route.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET!, {
   typescript: true,
-  apiVersion: '2024-04-10',
+  apiVersion: '2024-06-20',
 })
 
 export async function GET() {
